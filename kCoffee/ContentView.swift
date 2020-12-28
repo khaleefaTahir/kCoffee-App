c95f8839352af0ac941884baf62a8f01daddc6b1
Mohammed Sani Hassan <hassan.smohammed@aun.edu.ng>
2020-12-28 21:43:12 +0100

    Initial Commit

---
+++ kCoffee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kCoffee
+//
+//  Created by mohammed sani hassan on 28/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
